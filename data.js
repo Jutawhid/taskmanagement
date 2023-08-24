2f4a7c29bcff47b10b00f8f61880bfdc726ae478
jusim@medinatech.co <jusim@medinatech.co>
2023-08-24 17:36:22 +0600

    1st commit

---
+++ data.js
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from "uuid";
+const initdata = [
+  {
+    id: uuidv4(),
+    boardName: "💡 Todo",
+    card: [
+    ],
+  },
+  {
+    id: uuidv4(),
+    boardName: "⏳ In Progess",
+    card: [
+    ],
+  },
+  {
+    id: uuidv4(),
+    boardName: "✅ Done",
+    card: [
+    ],
+  },
+];
+export default initdata;
